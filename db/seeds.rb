5310b3e3ab36a47c5676f744a3c561061dd163e3
Brian Ridolce <brianridolce@gmail.com>
2019-11-13 16:15:41 +0100

    message

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+Restaurant.destroy_all
+
+puts "Generating 5 restaurants from the darkweb"
+
+6.times do
+  restaurant = Restaurant.new(
+    name: Faker::Restaurant.name,
+    address: "#{Faker::Address.full_address}".truncate(10),
+    phone_number: "#{Faker::PhoneNumber.cell_phone}",
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+
+puts "Saving Restaurant to the DB"
+  restaurant.save!
+end
+puts "Finnishing the hacking of your DB, nice job dude"
